57b451e5c1b126969c808e9e579f3dbde1994b30
Amol Jadhav <amoljadhav2906@gmail.com>
2025-07-30 20:51:31 +0530

    Test Fork

--- amol.py
+++ amol.py
@@ -14,4 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Amol')
     print("I am an EnTc Engineer")
+    print("I am Testing Fork")
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
